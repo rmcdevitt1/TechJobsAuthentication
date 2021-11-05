1e37df4d5f8b4028dd19dcca3ef8a0ea817c68e9
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-11-05 14:41:24 -0500

    finished studio

--- TechJobsAuthentication/Startup.cs
+++ TechJobsAuthentication/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,8 +28,12 @@ namespace TechJobsAuthentication
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
-            //services.AddDbContext<JobDbContext>(options =>
-            //    options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            services.Configure<IdentityOptions>(options =>
+                {
+                  options.Password.RequiredLength = 8;
+                  options.Password.RequireUppercase = false;
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +54,7 @@ namespace TechJobsAuthentication
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
